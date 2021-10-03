cdfbd9a48dc194fdb83b88f8a554f3f167954088
Chetan Gupta <59758751+chetanguptamrt@users.noreply.github.com>
2021-10-03 21:43:22 +0530

    Update 8x8script.js

--- js/8x8script.js
+++ js/8x8script.js
@@ -43,6 +43,16 @@ for(let i=0; i<24; i++){
     }
 }
 
+//load all emojis
+async function loadImage() {
+    for(let i = 0; i<position.length; i++) {
+        $("#fargi").attr('src', "../img/"+position[i]+".png");
+        await sleep(100);
+    }
+  console.log("done");
+}
+loadImage();
+
 //give emojis to all position
 for(let i = 0; i<position.length; i++){
     $(".emoji-"+(i+1)).css('background-image', "url('../img/question.png')");
